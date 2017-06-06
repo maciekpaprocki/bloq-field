62de37b4a0dec884f8aea1521da9c7fa0b4be5cd
Maciej <maciekpaprocki@gmail.com>
2017-06-07 00:49:56 +0100

    added travis and started api examples

---
+++ doc/blocks/Image/Block.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ExampleBlock;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2017 maciejpaprocki
+ */
+class Block
+{
+    /**
+     * @var string
+     */
+    public $src;
+    /**
+     * @var LoggerInterface
+     */
+    private $_logger;
+
+    /**
+     * Block constructor.
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger, )
+    {
+        $this->_logger = $logger;
+    }
+
+    public function render()
+    {
+
+    }
+
+    public function getCacheKey()
+    {
+
+    }
+
+
+}
